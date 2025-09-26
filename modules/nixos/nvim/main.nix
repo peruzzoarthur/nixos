fa33edb8188b6b7f28613b2d49c87e370b116572
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2025-09-26 17:00:47 -0300

    no autocmd for now

--- modules/nixos/nvim/main.nix
+++ modules/nixos/nvim/main.nix
@@ -5,23 +5,23 @@
       vim.viAlias = false;
       vim.vimAlias = true;
       
-      vim.autocmds = [
-        {
-          enable = true;
-          event = ["User"];
-          pattern = ["LazyDone"];
-          callback = lib.generators.mkLuaInline ''
-            function()
-              vim.defer_fn(function()
-                pcall(function()
-                  vim.cmd("ZenMode")
-                end)
-              end, 100)
-            end
-          '';
-          desc = "Auto-enable zen mode after plugins load";
-        }
-      ];
+      # vim.autocmds = [
+      #   {
+      #     enable = true;
+      #     event = ["User"];
+      #     pattern = ["LazyDone"];
+      #     callback = lib.generators.mkLuaInline ''
+      #       function()
+      #         vim.defer_fn(function()
+      #           pcall(function()
+      #             vim.cmd("ZenMode")
+      #           end)
+      #         end, 100)
+      #       end
+      #     '';
+      #     desc = "Auto-enable zen mode after plugins load";
+      #   }
+      # ];
     };
   };
 }
