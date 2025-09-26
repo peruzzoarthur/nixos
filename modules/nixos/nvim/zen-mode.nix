31589f9b5062dadc1d81e1652366d9dc33b4db02
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2025-09-26 16:56:22 -0300

    trying to start nvim in zenmode

--- modules/nixos/nvim/zen-mode.nix
+++ modules/nixos/nvim/zen-mode.nix
@@ -1,38 +1,27 @@
-{pkgs, ...}:
-{
-programs.nvf = {
-  settings = {
-  config.vim.lazy.plugins = {
-    "zen-mode.nvim" = {
-      package = pkgs.vimPlugins.zen-mode-nvim;
-      setupModule = "zen-mode";
-      setupOpts = {
-        window = {
-          backdrop = 0.95;
-          width = 120;
-          height = 1;
+{pkgs, ...}: {
+  programs.nvf = {
+    settings = {
+      config.vim.lazy.plugins = {
+        "zen-mode.nvim" = {
+          package = pkgs.vimPlugins.zen-mode-nvim;
+          setupModule = "zen-mode";
+          setupOpts = {
+            window = {
+              backdrop = 0.95;
+              width = 120;
+              height = 1;
+            };
+          };
+          keys = [
+            {
+              key = "<leader>z";
+              action = ":ZenMode<CR>";
+              mode = "n";
+              desc = "Toggle Zen Mode";
+            }
+          ];
         };
       };
-      after = ''
-        -- Auto-enable zen mode on startup
-        vim.api.nvim_create_autocmd("VimEnter", {
-          callback = function()
-            vim.cmd("ZenMode")
-          end,
-        })
-      '';
-
-      # load on keymap
-      keys = [
-        {
-          key = "<leader>z";
-          action = ":ZenMode<CR>";
-          mode = "n";
-          desc = "Toggle Zen Mode";
-        }
-      ];
     };
   };
-  };
- };
 }
