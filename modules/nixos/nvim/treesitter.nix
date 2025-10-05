227dee1122c315d17f475784f7c9608d66ed8441
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2025-10-05 11:08:06 -0300

    added prisma grammars in treesitter

--- modules/nixos/nvim/treesitter.nix
+++ modules/nixos/nvim/treesitter.nix
@@ -1,8 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   programs.nvf = {
     settings = {
       vim.treesitter = {
         enable = true;
+        grammars = [
+          pkgs.vimPlugins.nvim-treesitter.builtGrammars.prisma
+        ];
+        indent.enable = true;
       };
       vim.languages.enableTreesitter = true;
     };
