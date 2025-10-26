8f097bfe7eb8d83f82551f40a0ffac0f989bf500
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2025-10-26 11:37:11 -0300

    added hyprfocus

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -29,6 +29,7 @@
     plugins = [
       inputs.hyprland-plugins.packages.${pkgs.system}.hyprscrolling
       inputs.hyprland-plugins.packages.${pkgs.system}.csgo-vulkan-fix
+      inputs.hyprland-plugins.packages.${pkgs.system}.hyprfocus
     ];
     settings = {
       # Monitor configuration
@@ -96,8 +97,12 @@
 
       # Animations
       animations = {
-        enabled = false;
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        enabled = true;
+        bezier = [
+          "myBezier, 0.05, 0.9, 0.1, 1.05"
+          "bezIn, 0.5,0.0,1.0,0.5"
+          "bezOut, 0.0,0.5,0.5,1.0"
+        ];
         animation = [
           "windows, 1, 7, myBezier"
           "windowsOut, 1, 7, default, popin 80%"
@@ -158,6 +163,37 @@
         focus_fit_method = 0;
       };
 
+      "plugin:hyprfocus" = {
+        enabled = true;
+        
+        keyboard_focus_animation = "shrink";
+        mouse_focus_animation = "flash";
+        
+        bezier = [
+          "bezIn, 0.5,0.0,1.0,0.5"
+          "bezOut, 0.0,0.5,0.5,1.0"
+        ];
+        
+        flash = {
+          flash_opacity = 0.7;
+          
+          in_bezier = "bezIn";
+          in_speed = 0.5;
+          
+          out_bezier = "bezOut";
+          out_speed = 3;
+        };
+        
+        shrink = {
+          shrink_percentage = 0.8;
+          
+          in_bezier = "bezIn";
+          in_speed = 0.5;
+          
+          out_bezier = "bezOut";
+          out_speed = 3;
+        };
+      };
       # Variables
       "$mainMod" = "SUPER";
 
