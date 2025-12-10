5c8d050cb66f7c4b23461fdfb8d6a4a21b85cbc5
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2025-12-10 16:15:19 -0300

    fixed correct profile path for firefox

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }: {
   programs.firefox = {
     enable = true;
-    profiles.default = {
+    profiles.ozzu = {
       isDefault = true;
+      path = "0f33li1b.ozzu";
       settings = {
         "widget.use-xdg-desktop-portal.file-picker" = 1;
         "widget.use-xdg-desktop-portal.open-uri" = 1;
