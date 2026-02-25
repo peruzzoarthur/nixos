dd34bb68cabc8d1f53206254f78b384a43a531de
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2026-02-25 09:21:09 -0300

    added fnm to nushell

--- modules/home-manager/nushell/default.nix
+++ modules/home-manager/nushell/default.nix
@@ -18,7 +18,10 @@
           | prepend $"($env.HOME)/.local/bin"
           | prepend $"($env.HOME)/.bun/bin"
           | prepend $"($env.HOME)/.pulumi/bin"
+          | prepend $"($env.HOME)/.fnm"
           | append "/usr/bin")
+        load-env (fnm env --shell bash | lines | str replace 'export ' "" | str replace -a '"' "" | split column "=" | rename name value | where name != "FNM_ARCH" and name != "PATH" | reduce -f {} {|it, acc| $acc | upsert $it.name $it.value })
+        $env.PATH = ($env.PATH | prepend $"($env.FNM_MULTISHELL_PATH)/bin")
       '';
     };
     configFile = {
