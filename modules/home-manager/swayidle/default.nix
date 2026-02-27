3fc837110de453e83c58997ab2ad6bff133e6330
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2026-02-27 12:29:50 -0300

    swayidle now turns monitors off and updated limine wp

--- modules/home-manager/swayidle/default.nix
+++ modules/home-manager/swayidle/default.nix
@@ -1,19 +1,27 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   noctalia-ipc = "${lib.getExe config.programs.noctalia-shell.package} ipc call";
+  niri = lib.getExe pkgs.niri;
 in {
   services.swayidle = {
     enable = true;
 
     systemdTarget = "graphical-session.target";
 
+    extraArgs = ["-w"];
+
     timeouts = [
       {
         timeout = 300;
         command = "${noctalia-ipc} lockScreen lock";
+      }
+      {
+        timeout = 400;
+        command = "${niri} msg action power-off-monitors";
       }
       {
         timeout = 900;
