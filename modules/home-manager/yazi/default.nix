6b33d2d9855403f3c7fe0a5a682702cbdc814f44
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2026-03-14 08:40:48 -0300

    fixed yazi config with theme

--- modules/home-manager/yazi/default.nix
+++ modules/home-manager/yazi/default.nix
@@ -10,13 +10,20 @@
       mgr = {
         ratio = [2 2 4];
         show_hidden = false;
-        sort_by = "mtime";
+        sort_by = "natural";
         sort_dir_first = true;
-        sort_reverse = true;
+        sort_reverse = false;
+        sorte_sensitive = true;
       };
     };
     flavors = {
       vague = ./vague-yazi;
+    };
+    theme = {
+      flavor = {
+        dark = "vague";
+        light = "vague";
+      };
     };
   };
 }
