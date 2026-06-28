7d02d385f127e248da63db32ecdd2326f6ff1bbb
Arthur Sperotto Peruzzo <peruzzoarthur@gmail.com>
2026-06-28 12:26:47 -0300

    fix atuin launch for DIRECTORY

--- modules/home-manager/fzf/default.nix
+++ modules/home-manager/fzf/default.nix
@@ -2,6 +2,8 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    # nushell auto-enables this; its Ctrl-R widget is sourced after atuin and steals the key. atuin owns history.
+    enableNushellIntegration = false;
     tmux = {
       enableShellIntegration = true;
     };
